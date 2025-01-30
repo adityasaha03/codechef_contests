2aa4806f739ce4b3d9f7c3db7e9cfedcfc7c045b
Aditya Saha <77659979+adityasaha03@users.noreply.github.com>
2025-01-31 03:01:14 +0600

    Update swish game.cpp

--- Starters/starter171/swish game.cpp
+++ Starters/starter171/swish game.cpp
@@ -13,14 +13,9 @@ int main() {
 
         for(int j=0; j<m;j++)
         {
-            if(moves[j]=='S')
-            {
-                c_s++;       //c_s is the counter variable for number of "S"
-            }
-            else
-            {
-                c_p++;      //c_p is the counter variable for number of "p"
-            }
+           	c_s+=(moves[j]=='S');
+        	c_p+=(moves[j]=='P');
+            
         }
 
         if(c_s>=k)
@@ -34,7 +29,10 @@ int main() {
         cout<<n<<endl;
     }
 
-}// the game ends for two reasons, if all rings are already used and if it becomes impossible to win,
+}
+//c_p is the counter variable for number of "p"
+//c_s is the counter variable for number of "S"
+// the game ends for two reasons, if all rings are already used and if it becomes impossible to win,
 // but not if the game is already won.So if the game is won the game continues until all rings are
 //used and thus number of moves=number of rings.
 
